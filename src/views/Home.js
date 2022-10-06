94d78291d027c01279c1e9d8a3cb9719786cde3b
EngrChibest <ibechinazam101@gmail.com>
2022-10-07 00:19:57 +0100

    services section home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,6 +3,7 @@ import { Divider } from '@mui/material';
 // import sections
 import Hero from '../components/sections/home/Hero';
 import Beneficts from '../components/sections/home/Beneficts'
+import Services from '../components/sections/home/Services'
 
 import LayoutDefault from '../layouts/LayoutDefault'
 
@@ -12,6 +13,7 @@ const Home = () => {
     <LayoutDefault>
       <Hero className="illustration-section-01" />
       <Beneficts/>
+      <Services/>
     </LayoutDefault>
   );
 }
