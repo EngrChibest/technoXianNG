55ace625c71c3b4523cb85df657c4a21a73301cb
EngrChibest <ibechinazam101@gmail.com>
2022-10-07 18:07:42 +0100

    added newsletter section

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,6 +6,7 @@ import Beneficts from '../components/sections/home/Beneficts'
 import Services from '../components/sections/home/Services'
 import Partners from '../components/sections/home/Partners';
 import Table from '../components/sections/home/Table';
+import Newsletter from '../components/sections/home/Newsletter';
 
 import LayoutDefault from '../layouts/LayoutDefault'
 
@@ -18,6 +19,7 @@ const Home = () => {
       <Services/>
       <Partners/>
       <Table/>
+      <Newsletter/>
     </LayoutDefault>
   );
 }
