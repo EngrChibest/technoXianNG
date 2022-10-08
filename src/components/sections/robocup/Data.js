b3e83e00ca8272274dc4afbbb22f7e4307ca47da
EngrChibest <ibechinazam101@gmail.com>
2022-10-09 00:47:44 +0100

    added robo competition page

---
+++ src/components/sections/robocup/Data.js
@@ -0,0 +1,64 @@
+import RoboRace from './../../../assets/images/features-split-image-03.png'
+
+export default [
+    {
+        id: 1,
+        face: true,
+        tag: 'TECHNOXIAN',
+        title: 'Hybrid Learning Model',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Amet risus, nunc id vitae sit imperdiet donec. Maecenas nibh 
+        fringilla neque tellus eget eget. Nisl suspendisse auctor ipsum 
+        posuere enim et sit egestas imperdiet.`,
+        img: RoboRace,
+        link: 'https://www.technoxian.com/event/robosoccer/'
+    },
+    {
+        id: 2,
+        face: false,
+        tag: 'TECHNOXIAN',
+        title: 'Hybrid Learning Model',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Amet risus, nunc id vitae sit imperdiet donec. Maecenas nibh 
+        fringilla neque tellus eget eget. Nisl suspendisse auctor ipsum 
+        posuere enim et sit egestas imperdiet.`,
+        img: RoboRace,
+        link: 'https://www.technoxian.com/event/robosoccer/'
+    },
+    {
+        id: 3,
+        face: true,
+        tag: 'TECHNOXIAN',
+        title: 'Hybrid Learning Model',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Amet risus, nunc id vitae sit imperdiet donec. Maecenas nibh 
+        fringilla neque tellus eget eget. Nisl suspendisse auctor ipsum 
+        posuere enim et sit egestas imperdiet.`,
+        img: RoboRace,
+        link: 'https://www.technoxian.com/event/robosoccer/'
+    },
+    {
+        id: 4,
+        face: false,
+        tag: 'TECHNOXIAN',
+        title: 'Hybrid Learning Model',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Amet risus, nunc id vitae sit imperdiet donec. Maecenas nibh 
+        fringilla neque tellus eget eget. Nisl suspendisse auctor ipsum 
+        posuere enim et sit egestas imperdiet.`,
+        img: RoboRace,
+        link: 'https://www.technoxian.com/event/robosoccer/'
+    },
+    {
+        id: 5,
+        face: true,
+        tag: 'TECHNOXIAN',
+        title: 'Hybrid Learning Model',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Amet risus, nunc id vitae sit imperdiet donec. Maecenas nibh 
+        fringilla neque tellus eget eget. Nisl suspendisse auctor ipsum 
+        posuere enim et sit egestas imperdiet.`,
+        img: RoboRace,
+        link: 'https://www.technoxian.com/event/robosoccer/'
+    },
+]
---
+++ src/components/sections/robocup/ServiceCard.js
@@ -0,0 +1,72 @@
+import { Grid, Button } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import Image from "../../elements/Image";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import styled from "styled-components";
+
+
+const LearnLink = styled(Button)({
+    textTransform: 'capitalize',
+    fontSize: '16px',
+})
+
+export default function ServicesCard ({image, tag, title, body, link, face}){
+    return(
+        <React.Fragment>
+        { face === true ? 
+            <Grid container spacing={5} sx={{my:'15%'}}>
+            <Grid item xs={12} sm={6} md={6}>
+                <Image
+                src={image}
+                alt="Hero" 
+                />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+                <p className="m-0 reveal-from-bottom primary-color" data-reveal-delay="400"><b>{tag}</b></p>
+                <h3 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+                    {title}
+                </h3>
+                <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                    {body} 
+                </p>
+                <div className="reveal-from-bottom" data-reveal-delay="600">
+                    <a href={link} target='_blank' rel="noopener noreferrer">
+                        <LearnLink variant="text">
+                            Learn More
+                            <ChevronRightIcon fontSize="small"/>
+                        </LearnLink>
+                    </a>
+                </div>
+            </Grid>
+        </Grid>
+        :
+        <Grid container spacing={5} sx={{my:'10%'}}>
+        <Grid item xs={12} sm={6} md={6}>
+            <p className="m-0 reveal-from-bottom primary-color" data-reveal-delay="400"><b>TECHNOXIAN</b></p>
+            <h3 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+            {title}
+            </h3>
+            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+            {body}
+            </p>
+            <div className="reveal-from-bottom" data-reveal-delay="600">
+                <a href={link} target='_blank' rel="noopener noreferrer">
+                    <LearnLink variant="text">
+                        Learn More
+                        <ChevronRightIcon fontSize="small"/>
+                    </LearnLink>
+                </a>
+            </div>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+            <Image
+                src={image}
+                alt="Hero" 
+                />
+        </Grid>
+        </Grid>
+        }
+        </React.Fragment>
+    )
+}
