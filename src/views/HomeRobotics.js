b92bf81e5ec9add78571f25802460797c6e2b0f8
EngrChibest <ibechinazam101@gmail.com>
2022-10-08 17:43:42 +0100

    added services to home Robo page

--- src/views/HomeRobotics.js
+++ src/views/HomeRobotics.js
@@ -2,12 +2,14 @@ import React from "react";
 import LayoutDefault from '../layouts/LayoutDefault'
 import Hero from "../components/sections/homerobot/Hero";
 import Beneficts from "../components/sections/homerobot/Beneficts";
+import Services from "../components/sections/homerobot/Services";
 
 export default function HomeRobotics () {
     return (
         <LayoutDefault>
           <Hero/>
           <Beneficts/>
+          <Services/>
       </LayoutDefault>
     )
 }
