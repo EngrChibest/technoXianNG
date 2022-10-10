d42663fabe9a871b505401f7679236687dd5f672
EngrChibest <ibechinazam101@gmail.com>
2022-10-10 16:32:15 +0100

    added FAQ page

---
+++ src/components/sections/faqs/Accordion.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function AccordionQuestion({title, description, panelNo, expanded, handleChange}) {
+  return (
+    <div>
+      <Accordion expanded={expanded === panelNo} onChange={handleChange} data-aos="fade-up">
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls= {`${panelNo}a-content`}
+          id={`${panelNo}a-header`}
+          sx={{paddingLeft: '3%', backgroundColor: '#E5E7EC',}}
+        >
+          <Typography sx={{color: '#00A859', fontWeight: 700}}>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{backgroundColor: '#E5E7EC', p: 4}}>
+          <Typography>
+            {description}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/views/FAQs.js
+++ src/views/FAQs.js
@@ -1,9 +1,13 @@
 import React from "react";
 import LayoutDefault from '../layouts/LayoutDefault'
+import Hero from "../components/sections/faqs/Hero";
+import FAQ from "../components/sections/faqs/FAQquestions";
 
 export default function FAQs () {
     return (
         <LayoutDefault>
+          <Hero/>
+          <FAQ/>
       </LayoutDefault>
     )
 }
