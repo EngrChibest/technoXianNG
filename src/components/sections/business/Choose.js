889d2f2538717e51a13e3fe9851010146d765977
EngrChibest <ibechinazam101@gmail.com>
2022-10-10 12:11:55 +0100

    added busniess page

---
+++ src/components/sections/business/Choose.js
@@ -0,0 +1,46 @@
+import { Card, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import Groups2Icon from '@mui/icons-material/Groups2';
+
+
+export default function Choose (){
+    return (
+        <div className="container-sm">
+            <h3 style={{textTransform: 'capitalize', marginTop: '16%'}}>Why Choose Us</h3>
+            <Grid container spacing={5} sx={{my:'4%'}}>
+                <Grid item xs={12} sm={3} md={3}>
+                    <Card className="benefict-card-two">
+                        <Groups2Icon fontSize="large" sx={{ color: '#00A859'}}/>
+                        <Typography variant="subtitle2" color="text.secondary">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet risus
+                        </Typography>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={3} md={3}>
+                    <Card className="benefict-card-two">
+                        <Groups2Icon fontSize="large" sx={{ color: '#00A859'}}/>
+                        <Typography variant="subtitle2" color="text.secondary">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet risus
+                        </Typography>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={3} md={3}>
+                    <Card className="benefict-card-two">
+                        <Groups2Icon fontSize="large" sx={{ color: '#00A859'}}/>
+                        <Typography variant="subtitle2" color="text.secondary">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet risus
+                        </Typography>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={3} md={3}>
+                    <Card className="benefict-card-two">
+                        <Groups2Icon fontSize="large" sx={{ color: '#00A859'}}/>
+                        <Typography variant="subtitle2" color="text.secondary">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet risus
+                        </Typography>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
--- src/views/Enterpreneurship.js
+++ src/views/Enterpreneurship.js
@@ -1,9 +1,17 @@
 import React from "react";
 import LayoutDefault from '../layouts/LayoutDefault'
+import Hero from "../components/sections/business/Hero";
+import Beneficts from "../components/sections/business/Beneficts";
+import Services from "../components/sections/business/Services";
+import Choose from "../components/sections/business/Choose";
 
 export default function Enterpreneurship () {
     return (
         <LayoutDefault>
+          <Hero/>
+          <Beneficts/>
+          <Services/>
+          <Choose/>
       </LayoutDefault>
     )
 }
