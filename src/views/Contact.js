0470246e746b2c8ab63f1b39ea4ee64a0bf9c1fa
EngrChibest <ibechinazam101@gmail.com>
2022-10-10 16:39:21 +0100

    added contact page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
 import LayoutDefault from '../layouts/LayoutDefault'
+import Hero from '../components/sections/contact/Hero'
+import ContactDetail from '../components/sections/contact/ContactDetail'
 
 export default function Contact () {
     return (
         <LayoutDefault>
+          <Hero/>
+          <ContactDetail/>
       </LayoutDefault>
     )
 }
