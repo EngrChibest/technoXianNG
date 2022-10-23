91705b08cb1f815e927e5bc45410e64ce2afc6d8
EngrChibest <ibechinazam101@gmail.com>
2022-10-23 08:43:48 +0100

    update footer section

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -48,46 +48,76 @@ const Footer = ({
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={4} md={4}>
                   <Link to='/'>
-                  <img src={ Logo } width="200" alt="" className="float-righ"/>
+                    <img src={ Logo } width="170" alt="" className="float-righ" style={{marginBottom: '18px'}}/>
+                  </Link>
+                  <Link to='/'>
+                    <p>Home</p>
+                  </Link>
+                  <Link to='/about'>
+                    <p>About Us</p>
+                  </Link>
+                  <Link to='/faqs'>
+                    <p>FAQs</p>
+                  </Link>
+                  <Link to='/contact'>
+                    <p>Contact Us</p>
                   </Link>
                 </Grid>
                 <Grid item xs={12} sm={8} md={8}>
                   <Grid container spacing={4}>
                     <Grid item md={4} sm={4} xs={12}>
-                      <h5 style={{color: "#2E2F6E"}}>Our Services</h5>
-                      <Link to='/home-roboclub'>
-                        <p>Home Robotics Club</p>
-                      </Link>
-                      <Link to='/robotics-club'>
-                        <p>School Robotics Club</p>
-                      </Link>
-                      <Link to='/nxr-lab'>
-                        <p>NXR Laboratory</p>
-                      </Link>
+                      <h5>Challenges</h5>
+                      <a href='https://www.technoxian.com/event/robosoccer/' target='_blank' rel="noopener noreferrer">
+                        <p>ROBOSOCCER</p>
+                      </a>
+                      <a href='https://www.technoxian.com/event/wrc-bots-combat/' target='_blank' rel="noopener noreferrer">
+                        <p>ROBOWAR</p>
+                      </a>
+                      <a href='https://www.technoxian.com/event/robo-race/' target='_blank' rel="noopener noreferrer">
+                        <p>ROBORACE</p>
+                      </a>
+                      <a href='https://www.technoxian.com/event/fastest-line-follower/' target='_blank' rel="noopener noreferrer">
+                        <p>LINE FOLLOWER</p>
+                      </a>
+                      <a href='https://www.technoxian.com/event/water-rocket/' target='_blank' rel="noopener noreferrer">
+                        <p>WATER ROCKET</p>
+                      </a>
                     </Grid>
                     <Grid item md={4} sm={4} xs={12}>
-                      <h5 style={{color: "#2E2F6E"}}>Community</h5>
+                      <h5>Community</h5>
+                      <Link to='/home-roboclub'>
+                        <p>Technoxian WRC</p>
+                      </Link>
+                      <Link to='/robotics-club'>
+                        <p>TX RoboClub</p>
+                      </Link>
                       <Link to='/nxr-lab'>
                         <p>NXR Laboratory</p>
                       </Link>
                       <Link to='/corporate-club'>
-                        <p>Corporate Robo Club</p>
+                        <p>Corporate RoboClub</p>
                       </Link>
                       <Link to='/enterpreneurship'>
                         <p>Business Education</p>
                       </Link>
                     </Grid>
                     <Grid item md={4} sm={4} xs={12}>
-                      <h5 style={{color: "#2E2F6E"}}>About Us</h5>
-                      <Link to='/about'>
-                        <p>About</p>
-                      </Link>
-                      <Link to='/faqs'>
-                        <p>FAQs</p>
-                      </Link>
-                      <Link to='/contact'>
-                        <p>Contact Us</p>
-                      </Link>
+                    <h5>Contact Us</h5>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <p><a style={{color: '#727272'}} href='https://goo.gl/maps/GBFMVqU6aRVPsMNe9' target='_blank'>Abuja, Nigeria</a></p>
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <p><a style={{color: '#727272'}} href='mailto:info@technoxiannigeria.ng'>info@technoxiannigeria.ng</a></p>
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <p >
+                          <a style={{color: '#727272'}} href='tel:09160605054'>0916 060 5054</a><br/>
+                          {/* <a style={{color: '#727272'}} href='tel:09156444478'>0915 644 4478</a> */}
+                        </p>
+                      </Grid>
+                      <Grid item xs={12} sm={12} md={12}>
+                        <FooterSocial />
+                      </Grid>
                     </Grid>
                   </Grid>
                 </Grid>
@@ -95,26 +125,6 @@ const Footer = ({
               </Grid>
             </Box>
             <Box>
-            <h5>Contact Us</h5>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={4} md={4}>
-                  <FooterSocial />
-                </Grid>
-                <Grid item xs={12} sm={8} md={8}>
-                  <Grid container spacing={5}>
-                    <Grid item xs={12} sm={4} md={4}>
-                      <p><a style={{color: '#727272'}} href='https://goo.gl/maps/GBFMVqU6aRVPsMNe9' target='_blank'>Office address: Abuja, Nigeria</a></p>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={4}>
-                      <p><a style={{color: '#727272'}} href='mailto:info@technoxiannigeria.ng'>info@technoxiannigeria.ng</a></p>
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={4}>
-                      <p ><a style={{color: '#727272'}} href='tel:09160605054'>0916 060 5054</a><br/><a style={{color: '#727272'}} href='tel:09156444478'>0915 644 4478</a></p>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                
-              </Grid>
             </Box>
             <br/><br/>
               <div className="footer-copyright" style={{color: "#9CA9B3", textAlign: 'center'}}>&copy; 2022 TechnoXian Nigeria. All right reserved</div>
